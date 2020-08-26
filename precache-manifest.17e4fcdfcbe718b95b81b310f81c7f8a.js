2acfdf093b50a77113305ae1d37427492c52b2e1
Baguiri <baguiri9473@gmail.com>
2020-08-26 17:37:30 +0300

    Updates

---
+++ precache-manifest.17e4fcdfcbe718b95b81b310f81c7f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eecc537f10942bc3e5c547f5e76627",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "63f931fe2b82780d8501",
+    "url": "/todolist/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f220b245a879c3039ad6",
+    "url": "/todolist/static/css/main.38b5130c.chunk.css"
+  },
+  {
+    "revision": "63f931fe2b82780d8501",
+    "url": "/todolist/static/js/2.2b2a77ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.2b2a77ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f220b245a879c3039ad6",
+    "url": "/todolist/static/js/main.f2842347.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
